887491850529865276613ab6c38a4fc820715d74
Sujatha <svvsdpmsujatha02@gmail.com>
2026-08-27 10:36:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (62.53%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            s = 0
+            while n:
+                digit = n % 10
+                s += digit * digit
+                n //= 10
+            n = s
+        return True
